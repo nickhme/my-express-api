06a9b8b515b0b04f732cebb477be330efa2db463
Nick Hayes <nhayes559@gmail.com>
2025-01-13 14:40:33 +0000

    auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,10 +14,43 @@ router.get("/signup", (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
-    res.redirect('/')
+    res.redirect('/login')
   } catch (e) {
     next(e)
   }  
 });
+
+// TODO login
+// Login page (just like signup.ejs) ✅
+// GET /login controller to return our ejs page ✅
+// When you sign up, redirect to login ✅
+router.get('/login', (req, res, next) => {
+  try {
+    res.render("user/login.ejs");
+  } catch (e) {
+    next(e)
+  }  
+})
+
+// POST /login controller to handle POSTing the login.
+router.post('/login', async (req, res, next) => {
+  try {
+    // ? We need to know if the login was actually successful!
+    // 1) Get the user for this login attempt (with email)
+    const user = await User.findOne({ email: req.body.email })
+    // 2) Compare the 2 password hashes to see if they're the same.
+    // ! This will check if the login is a failure, and respond accordingly.
+    if (!user.isPasswordValid(req.body.password)) {
+      return res.status(401).send({ message: "Unauthorized"})
+    }
+
+    // If we succeed, we do this later:
+    // req.session.user = user
+    res.send({ message: "Login successful!"})
+
+  } catch(e) {
+    next(e)
+  }
+})
 
 export default router
--- models/user.js
+++ models/user.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
   username: {
     type: String,
     required: true,
@@ -10,6 +15,28 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// * Before the user document is created, we want to replace 
+// * the password with a hashed version.
+// mongoose has a lifecycle for each document, e.g. validation, saving etc.
+// this one runs before saving a document to the database.
+userSchema.pre('save', function(next) {
+  // 'this' refers to the doc you're about to save.
+  // this line replaces the password with the hashed password.
+  this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
+  next() // this tells mongoose we're done.
+})
+
+// * I need a function to compare the passwords and return true
+// * if the passwords match.
+// This will actually a method to all user documents.
+userSchema.methods.isPasswordValid = function(plaintextPassword) {
+  // Use bcrypt to check the 2 passwords
+  // ? Argument 1: password user is trying to log in with
+  // ? Argument 2: real existing hashed password for this user
+  return bcrypt.compareSync(plaintextPassword, this.password)
+}
+
 
 export default mongoose.model("User", userSchema);
 
